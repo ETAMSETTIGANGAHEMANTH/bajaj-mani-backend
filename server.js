6c3fb59de8e7d46ae26df865a974d493be7eb178
ETAMSETTIGANGAHEMANTH <103168797+ETAMSETTIGANGAHEMANTH@users.noreply.github.com>
2024-09-22 17:48:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const WebSocket = require('ws');
 const app = express();
 app.use(bodyParser.json());
 
-const allowedOrigins = ['https://bajaj-mani-frontend-7d18afbf56f7.herokuapp.com'];
+const allowedOrigins = ['https://bajaj-mani-frontend-7d18afbf56f7.herokuapp.com/bfhl'];
 
 app.use(cors({
     origin: function (origin, callback) {
